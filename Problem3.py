d8d423337baa7a43aa446546b6f2e83df505851c
enderman19980125 <enderman19980125@outlook.com>
2020-09-21 09:42:37 +0800

    Update Problem2-3 Oil Figure

--- Problem3.py
+++ Problem3.py
@@ -12,28 +12,47 @@ from Point import Point3d, distance3d
 
 # max_distance = 0.06963137125681296
 
+
 def plot_oil() -> None:
     data = pd.read_excel("Results.xlsx", "第三问结果").values[:, :7]
     x, y = [[], [], [], [], [], [], []], [[], [], [], [], [], [], []]
-    scale = 1.8
+    scale = 1.7
     for time, s1, s2, s3, s4, s5, s6 in data:
         time = int(time)
-        if s1 > 0:
+        if (len(y[1]) > 0 and y[1][-1] == 1) and s1 > 0:
+            x[1].append(time)
+            y[1].append(1)
+        if (len(y[2]) > 0 and y[2][-1] == 2) and s2 > 0:
+            x[2].append(time)
+            y[2].append(2)
+        if (len(y[3]) > 0 and y[3][-1] == 3) and s3 > 0:
+            x[3].append(time)
+            y[3].append(3)
+        if (len(y[4]) > 0 and y[4][-1] == 4) and s4 > 0:
+            x[4].append(time)
+            y[4].append(4)
+        if (len(y[5]) > 0 and y[5][-1] == 5) and s5 > 0:
+            x[5].append(time)
+            y[5].append(5)
+        if (len(y[6]) > 0 and y[6][-1] == 6) and s6 > 0:
+            x[6].append(time)
+            y[6].append(6)
+        if (len(y[1]) > 0 and y[1][-1] > 1) or s1 > 0:
             x[1].append(time)
             y[1].append(1 + s1 / scale)
-        if s2 > 0:
+        if (len(y[2]) > 0 and y[2][-1] > 2) or s2 > 0:
             x[2].append(time)
             y[2].append(2 + s2 / scale)
-        if s3 > 0:
+        if (len(y[3]) > 0 and y[3][-1] > 3) or s3 > 0:
             x[3].append(time)
             y[3].append(3 + s3 / scale)
-        if s4 > 0:
+        if (len(y[4]) > 0 and y[4][-1] > 4) or s4 > 0:
             x[4].append(time)
             y[4].append(4 + s4 / scale)
-        if s5 > 0:
+        if (len(y[5]) > 0 and y[5][-1] > 5) or s5 > 0:
             x[5].append(time)
             y[5].append(5 + s5 / scale)
-        if s6 > 0:
+        if (len(y[6]) > 0 and y[6][-1] > 6) or s6 > 0:
             x[6].append(time)
             y[6].append(6 + s6 / scale)
 
@@ -227,5 +246,5 @@ if __name__ == '__main__':
     # plot_track(ideal_barycenters_np_, None)
     # real_barycenters_np_ = calc(ideal_barycenters_np_, ideal_oil_consume_mass_np_)
     # plot_track(ideal_barycenters_np_[:real_barycenters_np_.shape[0]], real_barycenters_np_)
-    # plot_oil()
-    plot_oil_main_tank()
+    plot_oil()
+    # plot_oil_main_tank()
